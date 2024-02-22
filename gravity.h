ac983d6294b2aa1d406eceacd975d195bb539775
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-02-22 17:10:53 +0100

    Delete gravity.h

--- gravity.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAVITY_H
-#define GRAVITY_H
-
-#include "data.h"
-#include <cstdint>
-
-void Grav_Applay2Point(uint16_t PointX, uint16_t PointY, uint32_t mass, parts Particles);
-
-#endif // !GRAVITY_H
